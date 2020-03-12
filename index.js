7cddc3684afb202803cbe1a6325ac91927c040cd
David <chen7david@me.com>
2020-03-12 12:21:40 +0800

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+const validateHeader = (schema) => {
+    return (req, res, next) => {
+        const body = req.body
+        const { error, value } = schema.validate(body)
+        if(error) return next(error)
+        if(!req.ctx) req.ctx = {}
+        req.ctx.body = value
+        next()
+    }
+}
+
+const validateBody = (schema) => {
+    return (req, res, next) => {
+        const headers = req.headers
+        const { error, value } = schema.validate(headers)
+        if(error) return next(error)
+        if(!req.ctx) req.ctx = {}
+        req.ctx.headers = value
+        next()
+    }
+}
+
+module.exports = { validateHeader, validateBody }
