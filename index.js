1cc9d16fcecb01ea35473f765313c13ecb0731cd
David Chen <chen7david@me.com>
2020-03-23 12:46:27 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const validateBody = (schema) => {
-    return (req, res, next) => {
+const validateBody = (schema) => (req, res, next) => {
+    try {
         const body = req.body
         const { error, value } = schema.validate(body)
         if(error) return next(error)
         if(!req.ctx) req.ctx = {}
         req.ctx.body = value
         next()
+    } catch (err) {
+        next(err) 
     }
 }
 
-const validateHeader = (schema) => {
-    return (req, res, next) => {
+const validateHeader = (schema) => (req, res, next) => {
+    try {
         const headers = req.headers
         const { error, value } = schema.validate(headers)
         if(error) return next(error)
         if(!req.ctx) req.ctx = {}
         req.ctx.headers = value
         next()
+    } catch (err) {
+        next(err)
     }
 }
 
